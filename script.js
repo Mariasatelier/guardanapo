e4e027af3952b12450893903a0a093df9dbab251
Mariasatelier <91706311+Mariasatelier@users.noreply.github.com>
2022-02-09 11:20:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+var itemCmp = document.querySelector('.item-content');
+var imgItem = document.querySelector('.img-item');
+var titleItem = document.querySelector('.title-item');
+var valorItem = document.querySelector('.valor-item');
+var pacelaItem = document.querySelector('.pacela-item');
+var descricaoItem = document.querySelector('.descricao-item');
+var bxX = document.querySelector('.bx-x');
+var cdp = document.querySelector('.cdp');
+var body = document.querySelector('body');
+var item1 = document.querySelector('.item-1');
+var item2 = document.querySelector('.item-2');
+var item3 = document.querySelector('.item-3');
+var item4 = document.querySelector('.item-4');
+var item5 = document.querySelector('.item-5');
+var item6 = document.querySelector('.item-6');
+var item7 = document.querySelector('.item-7');
+var item8 = document.querySelector('.item-8');
+var item9 = document.querySelector('.item-9');
+var item10 = document.querySelector('.item-10');
+var item11 = document.querySelector('.item-11');
+var item12 = document.querySelector('.item-12');
+var item13 = document.querySelector('.item-13');
+var item14 = document.querySelector('.item-14');
+var item15 = document.querySelector('.item-15');
+var item16 = document.querySelector('.item-16');
+
+item1.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/JnZfx6DX/Whats-App-Image-2022-01-27-at-18-13-05-1-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com cor amarela"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com cor amarela para enfeitar seu prato."
+  cdp.value = "GIGO"
+});
+
+item2.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/vHQJ7vQ1/Whats-App-Image-2022-01-27-at-18-13-05-2-1-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com cor azul"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com cor azul para enfeitar seu prato."
+  cdp.value = "GIGI"
+});
+
+item3.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/MHYh2nYK/Whats-App-Image-2022-01-27-at-18-13-05-3-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo dde pérola"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo de pérola para enfeitar seu guardanapo."
+  cdp.value = "WELL"
+});
+
+item4.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/0jvLBYDB/Whats-App-Image-2022-01-27-at-18-13-05-4-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em anel com presentinho"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em anel com presentinho para endeitar seu guardanapo."
+  cdp.value = "AVAN"
+});
+
+item5.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/MpLgS21R/Whats-App-Image-2022-01-27-at-18-13-05-5-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em anel com um girassol"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em anel com um girassol para enfeitar seu guardanapo"
+  cdp.value = "IVNA"
+});
+
+item6.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/mrFJZXSn/Whats-App-Image-2022-01-27-at-18-13-06-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa florida"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa florida para enfeitar seeu guardanapo."
+  cdp.value = "AEEE"
+});
+
+item7.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/9Xt3qkj5/Whats-App-Image-2022-01-27-at-18-13-06-1-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina azul e branca"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina azul e branca pare enfeitar seu guardanapo."
+  cdp.value = "EAEA"
+});
+ 
+item8.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/d08Kn8rd/Whats-App-Image-2022-01-27-at-18-13-06-2-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina vermelha HOHO"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina vermelha HOHO para enfeitar seu guardanapo."
+  cdp.value = "KYGY"
+});
+
+item9.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/W3LKdGW1/Whats-App-Image-2022-01-27-at-18-15-20-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa em mandala"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa em mandala para enfeitar seu guardanapo"
+  cdp.value = "OIFI"
+});
+
+item10.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/P5J09rvT/Whats-App-Image-2022-01-27-at-18-15-20-1-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa listrada branca e rosa"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa listrada branca e rosa para enfeitar seu guardanapo"
+  cdp.value = "BESA"
+});
+
+item11.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/Dy8kRV17/Whats-App-Image-2022-01-27-at-18-15-20-2-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor branca"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor branca com tecido 100% algodão para enfeitar sua mesa"
+  cdp.value = "L0V3"
+});
+
+item12.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/J7TCT7DT/Whats-App-Image-2022-01-27-at-18-15-21-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor rosa claro"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor rosa claro com tecido 100% algodão para enfeitar sua mesa"
+  cdp.value = "UYYU"
+});
+
+item13.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/rsqPJ8L6/Whats-App-Image-2022-01-27-at-18-15-21-1-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor azul claro"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor rosa claro com tecido 100% algodão para enfeitar seu mesa."
+  cdp.value = "VBNC"
+});
+
+item14.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/FzGnC29Y/Whats-App-Image-2022-01-27-at-18-15-21-2-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor verde claro"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor verde claro com tecido 100% algodão para enfeitar sua mesa."
+  cdp.value = "LKJU"
+});
+
+item15.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/59mGtFwf/Whats-App-Image-2022-01-27-at-18-15-21-3-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor verde"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor verde para enfeitar sua mesa."
+  cdp.value = "DSAW"
+});
+
+item16.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/kXXHRXcK/Whats-App-Image-2022-01-27-at-18-15-21-4-removebg-preview.png"
+  titleItem.innerHTML = "Guardanapo de tecido com estampa de cor verde escuro"
+  valorItem.innerHTML = "R$10,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Guardanapo de tecido com estampa de cor verde escuro com tecido 100% algodão para enfeitar sua mesa."
+  cdp.value = "GRGR"
+});
